611f3ba53be23fbcc4af4159eb280705d30e3da5
vlad <v.shamraev1@yandex.ru>
2021-12-10 17:52:14 +0300

    First commit

---
+++ DungeonMaster.py
@@ -0,0 +1,54 @@
+import pygame
+
+
+class Board:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [[0] * width for i in range(height)]
+        self.cell_size = 30
+
+    def set_view(self, cell_size):
+        self.cell_size = cell_size
+
+    def render(self, surface):
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                pygame.draw.rect(surface, (255, 255, 255),
+                                 (ScreenWidth // 2 + self.cell_size * j - self.cell_size * self.width // 2,
+                                  self.cell_size * i + 50,
+                                  self.cell_size,
+                                  self.cell_size), 1)
+
+    def get_cell(self, pos):
+        x0 = ScreenWidth // 2 - self.cell_size * self.width // 2
+        y0 = 50
+        x = pos[0] - x0
+        y = pos[1] - y0
+        j = x // self.cell_size + 1
+        i = y // self.cell_size + 1
+        if 1 <= i < self.height + 1 and 1 <= j < self.width + 1:
+            return j, i
+        else:
+            return None
+
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption('Dungeon Master')
+    size = ScreenWidth, ScreenHeight = 900, 700
+    screen = pygame.display.set_mode(size)
+
+running = True
+board = Board(9, 7)
+board.set_view(ScreenHeight // 10)
+screen.fill((0, 0, 0))
+board.render(screen)
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print(board.get_cell(event.pos))
+    pygame.display.flip()
