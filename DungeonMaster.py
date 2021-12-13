230af4f8fa9e7f24e7156a539b66342ec5741f42
vlad <v.shamraev1@yandex.ru>
2021-12-13 18:23:38 +0300

    filling the cells

--- DungeonMaster.py
+++ DungeonMaster.py
@@ -1,29 +1,51 @@
 import pygame
 import Rooms
 import random
+import pprint
 
 
 class Room:
     def __init__(self):
-        self.set_Rooms()
+        self.set_Room()
+        self.Cells = []
 
     def __str__(self):
         return str(self.Cells)
 
     def __getitem__(self, item):
-        return self.Cells[item]
+        return self.CellsTypes[item]
 
     def __setitem__(self, key, value):
-        self.Cells[key] = value
+        self.CellsTypes[key] = value
 
-    def set_Rooms(self):
-        self.Cells = random.choice(Rooms.Rooms)
+    def set_Room(self):
+        self.CellsTypes = random.choice([Rooms.Room1])
 
 
 class Cell:
-    def __init__(self, x, y):
+    def __init__(self, x, y, type):
         self.x = x
         self.y = y
+        self.type = type
+        self.visible = False
+
+    def set_Visible(self, visible):
+        self.visible = visible
+        if self.type == 'Stone':
+            DrawingCell = pygame.image.load("image/mount.png")
+        elif self.type == 'Enemy':
+            DrawingCell = pygame.image.load("image/enemy.png")
+        elif self.type == 'Chest':
+            DrawingCell = pygame.image.load("image/chest.png")
+        elif self.type == 'Potion':
+            DrawingCell = pygame.image.load("image/potions.png")
+        else:
+            DrawingCell = pygame.image.load("image/hidden_cell.png")
+        screen.blit(DrawingCell, (self.x, self.y))
+        pygame.display.update()
+
+    def __str__(self):
+        return self.type
 
 
 class Board:
@@ -36,11 +58,14 @@ class Board:
     def set_view(self, cell_size):
         self.cell_size = cell_size
 
-    def render(self, surface):
-        Cells = []
+    def render(self, surface, room):
         for i in range(len(self.board)):
+            room.Cells.append([])
             for j in range(len(self.board[i])):
-                Cells.append(Cell(j, i))
+                room.Cells[i].append(
+                    Cell(ScreenWidth // 2 + self.cell_size * j - self.cell_size * self.width // 2,
+                         self.cell_size * i + 50, room[i][j]))
+                room.Cells[i][j].set_Visible(True)
                 pygame.draw.rect(surface, (255, 255, 255),
                                  (ScreenWidth // 2 + self.cell_size * j - self.cell_size * self.width // 2,
                                   self.cell_size * i + 50,
@@ -66,12 +91,13 @@ if __name__ == '__main__':
     size = ScreenWidth, ScreenHeight = 900, 700
     screen = pygame.display.set_mode(size)
 
-running = True
 board = Board(9, 7)
 board.set_view(ScreenHeight // 10)
 screen.fill((0, 0, 0))
-board.render(screen)
+Room1 = Room()
+board.render(screen, Room1)
 running = True
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
