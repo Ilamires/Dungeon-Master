4a315d31bb430a7c240eca7bf7da7ac4bfa83acb
Kovalev Dmitriy <kov.ds.2k@gmail.com>
2021-12-22 18:59:52 +0300

    add animation breathing hero

--- items.py
+++ items.py
@@ -2,5 +2,7 @@
 
 Consumable_items = {"fireball": ["damage", 20, 3]}
 
-items = {"fireball": ["damage", 20, 3]}
+#  название : [атака, атака огнем, вампиризм%]
+items_sword = {"rusty sword": [5, 0, 0],
+               "fire sword": [10, 5, 10]}
 
